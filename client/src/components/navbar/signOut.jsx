5b67c46f065e42a055f7e759477e0b2959932cca
David Davies <David_Davies@mail.com>
2022-04-28 14:34:14 +0100

    #12 added the signout button in the navbar

---
+++ client/src/components/navbar/signOut.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import {SignOutBtn} from '../../styles/Buttons'
+import { useNavigate } from "react-router-dom";
+
+const LoginContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export function SignOut(props) {
+  const navigate = useNavigate();
+  return (
+    <LoginContainer>
+       <SignOutBtn onClick={() => navigate('/')}>Sign Out</SignOutBtn>
+    </LoginContainer>
+  );
+}
