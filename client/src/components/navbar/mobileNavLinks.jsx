86f89af987989b39f3ab16325a56ee276d6e35e5
David Davies <David_Davies@mail.com>
2022-04-28 14:33:24 +0100

    #12 added the mobile nav links and styles

---
+++ client/src/components/navbar/mobileNavLinks.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { SignOut } from "./signOut";
+import { MenuToggle } from "./menuToggle";
+import { Link } from "react-router-dom";
+
+const NavLinksContainer = styled.div`
+  height: 100%;
+  display: flex;
+  z-index: 99;
+  color: var(--color-white);
+`;
+
+
+const LinksUl = styled.ul`
+  margin:0;
+  padding: 0;
+  display: flex;
+  height: 100%;
+  list-style: none;
+  background-color: var(--color-bluedark-varient);
+  width: 100%;
+  flex-direction: column;
+  position: fixed;
+  top: 6.2rem;
+  left: 0;
+`;
+
+const LinkItem = styled.li`
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+  font-weight: 500;
+  font-size: 2.4rem;
+  display: flex;
+  margin-bottom: 1rem;
+  
+
+  border-bottom: 2px solid transparent;
+  transition: all 240ms ease-in-out;
+  &:hover {
+    border-bottom: 2px solid #00c9fc;
+
+  }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+  font-size: inherit;
+  &:hover {
+    color: #ffdd19;
+    
+  }
+`;
+
+export function MobileLinks(props) {
+  const [isOpen, setOpen] = useState(true);
+
+  return (
+    <NavLinksContainer>
+      <MenuToggle isOpen={isOpen} toggle={() => setOpen(!isOpen)} />
+      {isOpen && (
+        <LinksUl>
+          <LinkItem>
+            <StyledLink to="/ttdashboard">My Dashboard</StyledLink>
+          </LinkItem>
+          <LinkItem>
+            <StyledLink to="/subject">Subject Selection</StyledLink>
+          </LinkItem>
+          <LinkItem>
+            <StyledLink to="/store">School Store</StyledLink>
+          </LinkItem>
+          <LinkItem>
+            <StyledLink to="/contact">Contact Us</StyledLink>
+          </LinkItem>
+
+          <SignOut />
+        </LinksUl>
+      )}
+    </NavLinksContainer>
+  );
+}
