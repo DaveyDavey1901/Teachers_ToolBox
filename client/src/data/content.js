32dec1c3b9c4f652f99ca2f4a823eb0b69bef586
David Davies <David_Davies@mail.com>
2022-04-29 22:09:21 +0100

    added some hardcoded data just to get things working for now

---
+++ client/src/data/content.js
@@ -0,0 +1,93 @@
+
+export const dataTeaching = [
+  {
+    name: "Teaching Resources",
+    review:
+      "lLorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aliquam tempore, veniam, culpa praesentium illum placeat voluptate corrupti deserunt earum facere! In quidem praesentium, maxime vero quis at tenetur facilis.",
+  },
+  {
+    name: "Two",
+    review:
+      "lLorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aliquam tempore, veniam, culpa praesentium illum placeat voluptate corrupti deserunt earum facere! In quidem praesentium, maxime vero quis at tenetur facilis.",
+  },
+  {
+    name: "Three",
+    review:
+      "lLorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aliquam tempore, veniam, culpa praesentium illum placeat voluptate corrupti deserunt earum facere! In quidem praesentium, maxime vero quis at tenetur facilis.",
+  },
+  {
+    name: "Four",
+    review:
+      "lLorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aliquam tempore, veniam, culpa praesentium illum placeat voluptate corrupti deserunt earum facere! In quidem praesentium, maxime vero quis at tenetur facilis.",
+  },
+];
+
+
+export const dataActivity = [
+  {
+    name: "Learning Activities",
+    review:
+      "lLorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aliquam tempore, veniam, culpa praesentium illum placeat voluptate corrupti deserunt earum facere! In quidem praesentium, maxime vero quis at tenetur facilis.",
+  },
+  {
+    name: "Two",
+    review:
+      "lLorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aliquam tempore, veniam, culpa praesentium illum placeat voluptate corrupti deserunt earum facere! In quidem praesentium, maxime vero quis at tenetur facilis.",
+  },
+  {
+    name: "Three",
+    review:
+      "lLorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aliquam tempore, veniam, culpa praesentium illum placeat voluptate corrupti deserunt earum facere! In quidem praesentium, maxime vero quis at tenetur facilis.",
+  },
+  {
+    name: "Four",
+    review:
+      "lLorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aliquam tempore, veniam, culpa praesentium illum placeat voluptate corrupti deserunt earum facere! In quidem praesentium, maxime vero quis at tenetur facilis.",
+  },
+];
+
+export const dataTesting = [
+  {
+    name: "Testing",
+    review:
+      "lLorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aliquam tempore, veniam, culpa praesentium illum placeat voluptate corrupti deserunt earum facere! In quidem praesentium, maxime vero quis at tenetur facilis.",
+  },
+  {
+    name: "Two",
+    review:
+      "lLorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aliquam tempore, veniam, culpa praesentium illum placeat voluptate corrupti deserunt earum facere! In quidem praesentium, maxime vero quis at tenetur facilis.",
+  },
+  {
+    name: "Three",
+    review:
+      "lLorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aliquam tempore, veniam, culpa praesentium illum placeat voluptate corrupti deserunt earum facere! In quidem praesentium, maxime vero quis at tenetur facilis.",
+  },
+  {
+    name: "Four",
+    review:
+      "lLorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aliquam tempore, veniam, culpa praesentium illum placeat voluptate corrupti deserunt earum facere! In quidem praesentium, maxime vero quis at tenetur facilis.",
+  },
+];
+
+export const dataStore = [
+  {
+    name: "School Store",
+    review:
+      "lLorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aliquam tempore, veniam, culpa praesentium illum placeat voluptate corrupti deserunt earum facere! In quidem praesentium, maxime vero quis at tenetur facilis.",
+  },
+  {
+    name: "Two",
+    review:
+      "lLorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aliquam tempore, veniam, culpa praesentium illum placeat voluptate corrupti deserunt earum facere! In quidem praesentium, maxime vero quis at tenetur facilis.",
+  },
+  {
+    name: "Three",
+    review:
+      "lLorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aliquam tempore, veniam, culpa praesentium illum placeat voluptate corrupti deserunt earum facere! In quidem praesentium, maxime vero quis at tenetur facilis.",
+  },
+  {
+    name: "Four",
+    review:
+      "lLorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aliquam tempore, veniam, culpa praesentium illum placeat voluptate corrupti deserunt earum facere! In quidem praesentium, maxime vero quis at tenetur facilis.",
+  },
+];
