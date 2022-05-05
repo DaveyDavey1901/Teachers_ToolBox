90bb6957a21aa051c74cf159a248d6899ee40775
David Davies <David_Davies@mail.com>
2022-05-05 01:09:15 +0100

    #23 added the index login and signup components

---
+++ client/src/components/formBoxFile/loginForm.jsx
@@ -0,0 +1,87 @@
+import { useContext, useState } from "react";
+import {
+  BoldLink,
+  BoxContainer,
+  FormContainer,
+  Input,
+  FadedLink,
+  SubmitButton,
+} from "./common";
+
+import { AccountContext } from "./accountContext";
+import { AuthContext } from "../../context/authContext";
+import { useForm } from "../../utils/hook";
+import { useMutation } from "@apollo/react-hooks";
+import { gql } from "graphql-tag";
+import { useNavigate } from "react-router-dom";
+
+const LOGIN_USER = gql`
+  mutation login($loginInput: LoginInput) {
+    loginUser(loginInput: $loginInput) {
+      email
+      username
+      token
+    }
+  }
+`;
+
+export function LoginForm(props) {
+  const { switchToSignup } = useContext(AccountContext);
+  let navigate = useNavigate();
+  const context = useContext(AuthContext);
+  const [errors, setErrors] = useState([]);
+
+  function loginUserCallback() {
+    loginUser();
+  }
+
+  const { onChange, onSubmit, values } = useForm(loginUserCallback, {
+    email: "",
+    password: "",
+  });
+
+  const [loginUser] = useMutation(LOGIN_USER, {
+    update(proxy, { data: { loginUser: userData } }) {
+      context.login(userData);
+      navigate("/TTDashboard");
+    },
+    onError({ graphQLErrors }) {
+      setErrors(graphQLErrors);
+    },
+    variables: { loginInput: values },
+  });
+  return (
+    <BoxContainer>
+      <FormContainer>
+        <Input
+          label="Email"
+          name="email"
+          placeholder="Email"
+          onChange={onChange}
+        />
+        <Input
+          label="Password"
+          name="password"
+          placeholder="Password"
+          onChange={onChange}
+        />
+      </FormContainer>
+
+      <FadedLink href="#">Forget your password contact us?</FadedLink>
+
+      {errors.map(function (error) {
+        return <div severity="error">{error.message}</div>;
+      })}
+      <SubmitButton variant="contained" onClick={onSubmit}>
+        Login
+      </SubmitButton>
+
+      <FadedLink href="#">
+        Don't have an accoun?{" "}
+        <BoldLink href="#" onClick={switchToSignup}>
+          Sign Up
+        </BoldLink>
+      </FadedLink>
+    </BoxContainer>
+  );
+}
---
+++ client/src/components/formBoxFile/signupForm.jsx
@@ -0,0 +1,101 @@
+import { useContext, useState } from "react";
+import {
+  BoldLink,
+  BoxContainer,
+  FormContainer,
+  Input,
+  FadedLink,
+  SubmitButton,
+} from "./common";
+
+import { AccountContext } from "./accountContext";
+
+import { AuthContext } from "../../context/authContext";
+import { useForm } from "../../utils/hook";
+import { useMutation } from "@apollo/react-hooks";
+
+import { gql } from "graphql-tag";
+import { useNavigate } from "react-router-dom";
+
+const REGISTER_USER = gql`
+  mutation Mutation($registerInput: RegisterInput) {
+    registerUser(registerInput: $registerInput) {
+      username
+      email
+      password
+      token
+    }
+  }
+`;
+
+export function SignUpForm(props) {
+  const { switchToSignin } = useContext(AccountContext);
+ const context = useContext(AuthContext);
+ let navigate = useNavigate();
+ const [errors, setErrors] = useState([]);
+
+ function registerUserCallback() {
+     registerUser();
+ }
+
+ const { onChange, onSubmit, values } = useForm(registerUserCallback, {
+   username: "",
+   email: "",
+   password: "",
+   confirmPassword: "",
+ });
+
+ const [registerUser] = useMutation(REGISTER_USER, {
+   update(proxy, { data: { registerUser: userData } }) {
+     context.logout(userData);
+     navigate("/");
+   },
+   onError({ graphQLErrors }) {
+     setErrors(graphQLErrors);
+   },
+   variables: { registerInput: values },
+ });
+
+  return (
+    <BoxContainer>
+      <FormContainer>
+        <Input
+          label="Username"
+          name="username"
+          placeholder="Username"
+          onChange={onChange}
+        />
+        <Input
+          label="Email"
+          name="email"
+          placeholder="Email"
+          onChange={onChange}
+        />
+        <Input
+          label="Password"
+          name="password"
+          placeholder="Password"
+          onChange={onChange}
+        />
+        <Input
+          label="Confirm password"
+          name="confirmPassword"
+          placeholder="Confirm Password"
+          onChange={onChange}
+        />
+      </FormContainer>
+      {errors.map(function (error) {
+        return <div severity="error">{error.message}</div>;
+      })}
+      <SubmitButton variant="contained" onClick={onSubmit}>
+        Sign Up
+      </SubmitButton>
+      <FadedLink href="#">
+        Already have an account?
+        <BoldLink href="#" onClick={switchToSignin}>
+          Login
+        </BoldLink>
+      </FadedLink>
+    </BoxContainer>
+  );
+}
