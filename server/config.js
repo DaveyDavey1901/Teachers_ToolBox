71fff1d81b60039aba3c37bef3515bd464544fc2
David Davies <David_Davies@mail.com>
2022-05-07 00:13:08 +0100

    updated server and config file

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://DDavies:kX7eNZtKy6yGPa2@ddcluster.9i9bk.mongodb.net/ttoolbox?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
