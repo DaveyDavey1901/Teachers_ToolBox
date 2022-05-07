ef2dbafc338ff432dce37747e9d06e2f9d717f46
David Davies <david_davies@mail.com>
2022-05-07 08:16:08 +0100

    Delete config.js

--- server/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  MONGODB:
-    "mongodb+srv://DDavies:kX7eNZtKy6yGPa2@ddcluster.9i9bk.mongodb.net/ttoolbox?retryWrites=true&w=majority",
-  SECRET_KEY: "some very secret key",
-};
